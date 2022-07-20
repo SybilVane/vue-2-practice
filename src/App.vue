6e29b40a3fe0a044612fce95633dedbb290d3871
Sybil Vane <vanessa.zgn@gmail.com>
2022-07-20 13:01:57 +0200

    formatted

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
   <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    />
+    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 2 + Vite" />
     <ConcatInput :title="8" has-to-show-subtitle @vane-click="showAlert" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-import ConcatInput from './components/ConcatInput.vue';
+import HelloWorld from './components/HelloWorld.vue'
+import ConcatInput from './components/ConcatInput.vue'
 
 export default {
   components: {
     HelloWorld,
     ConcatInput
-},
-methods: {
-  showAlert () {
-    alert('Vane Click!')
+  },
+  methods: {
+    showAlert() {
+      alert('Vane Click!')
+    }
   }
 }
-};
-
 </script>
 
 <style>
